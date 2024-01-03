0b6e79585f1176446bf266c806fb86d8bc2c74be
Alan Mellor <alan.mellor@bjss.com>
2024-01-03 19:37:55 +0000

    Place ship

--- grid/grid.go
+++ grid/grid.go
@@ -4,8 +4,19 @@ type Grid struct {
 	positions [7][7]string
 }
 
-const emptySpace string = ""
+const (
+	emptySpace = ""
+	shipToken string = "ship"
+)
 
 func NewGrid() *Grid {
 	return &Grid{}
+}
+
+func (g *Grid) PlaceShip( row int, col int ) {
+	g.positions[row][col] = shipToken
+}
+
+func (g Grid) isShipAt( row int, col int ) bool {
+	return g.positions[row][col] == shipToken
 }
--- grid/grid_test.go
+++ grid/grid_test.go
@@ -18,6 +18,19 @@ func TestCreatesEmptyGrid(t *testing.T){
 	}
 }
 
+func TestPlaceShip(t *testing.T) {
+	// Arrange
+	grid := NewGrid()
+
+	// Act
+	grid.PlaceShip(1, 2)
+
+	// Assert
+	if !grid.isShipAt(1, 2) {
+		t.Error("Ship not placed")
+	}
+}
+
 func isGridEmpty(g *Grid) bool {
 	for _, row := range g.positions {
 		for _, position := range row {
