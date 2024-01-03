aa7ff018e9caab3019b8ec2e6f4b3df75742fd21
Alan Mellor <alan.mellor@bjss.com>
2024-01-03 22:20:39 +0000

    TODO comments for next steps around Player

--- player/player_test.go
+++ player/player_test.go
@@ -16,3 +16,10 @@ func TestReportsPlayerName(t *testing.T) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
+
+/* 
+  TODO next tests: 
+  - Places ships on own grid (implies own grid)
+  - Take shot at opponent (implies link to opponent grid)
+  - PlayerHasWonWhenAllShipsSunk
+*/
