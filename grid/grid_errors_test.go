9c173c1c77ff3910794be67a9760e04526b2dd88
Alan Mellor <alan.mellor@bjss.com>
2024-01-03 20:56:50 +0000

    Refactor split our grid error tests

---
+++ grid/grid_errors_test.go
@@ -0,0 +1,51 @@
+package grid
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestCannotPlaceShipOnTopOfAnother(t *testing.T) {
+	// Arrange 
+	grid := NewGrid()
+	
+	grid.PlaceShip(1, 2)
+
+	// Act 
+	got := grid.PlaceShip(1, 2)
+
+	// Arrange
+	want := errors.New("ship already at location")
+
+	if got.Error() != want.Error() {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestReportsErrorTooManyShipsPlaced(t *testing.T) {
+	// Arrange - place maximum limit of 9 ships
+	grid := NewGrid()
+	
+	grid.PlaceShip(0, 0)
+	grid.PlaceShip(0, 1)
+	grid.PlaceShip(0, 2)
+
+	grid.PlaceShip(1, 0)
+	grid.PlaceShip(1, 1)
+	grid.PlaceShip(1, 2)
+
+	grid.PlaceShip(2, 0)
+	grid.PlaceShip(2, 1)
+	grid.PlaceShip(2, 2)
+
+	// Act
+	got := grid.PlaceShip(3, 0)
+
+	// Assert
+	want := errors.New("cannot place ship - limit reached")
+
+	if got.Error() != want.Error() {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
