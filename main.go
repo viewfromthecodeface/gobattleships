ddd5c10c2f2d1038d76117ca51dadeffae216a16
Alan Mellor <alan.mellor@bjss.com>
2024-01-06 20:58:59 +0000

    Removed spike code

--- main.go
+++ main.go
@@ -13,51 +13,5 @@ The player to first sink all their opponent's battleships is the winner
 */
 
 func main() {
-	// not implemented
-
-	// spike
-	// turns := Turns{}
-	// p1 := player.New(turns, "one")
-	// p2 := player.New(turns, "two")
-
-	// p1.Fire(2, 3) // causes p2.Incoming(2, 3)
-
-	// // assume shot hit
-	// p1.Fire(0, 0) // error not your turn
-
-	// // p2 shoots
-	// p2.Fire(99, 99) // invalid shot
-	// p2.Fire(1, 5) // p1.Incoming(1, 5) - miss - back to p1
-
-	// p2.Fire(0,0) - error not your turn
-	
-
-	// // I think Im stuck because of the conflict between web driver and console
-	// // console is a loop
-	// while ( gameInPlay ) {
-	// 	print(activePlayerName)
-	// 	get input from active player
-	// 	fire
-	// 	display result
-	// 	update active player
-	// }
-
-	// but web driver will have an API endpoint for each player on different device
-	// there will be a protocol for front end
-	// for each player:
-	//  while ( my turn )
-	//    on click of shot pos
-	//    player fire
-	//    display result on UI
-	//    update active player
-	//
-	// web API probably has
-	// - new game
-	// - is it my turn?
-	// - fire
-	// - result or error UI update
-	// - is game over?
-	// loop back to "is it my turn"
-	// will need some kind of "server push" for it being your turn
-	// maybe simple AJAX poll? websocket? long XHR?
+	// No Action
 }
