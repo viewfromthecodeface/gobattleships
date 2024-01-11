6bb088bb7849c8539d46d6ab34f5aa8058a4b2a3
Alan Mellor <alan.mellor@bjss.com>
2024-01-11 16:46:49 +0000

    Players place 9 ships each

--- game/game.go
+++ game/game.go
@@ -1,7 +1,13 @@
 package game
 
-type TextInput interface {
+import (
+	"battleships/player"
+	"fmt"
+	"strconv"
+)
 
+type TextInput interface {
+	Fetch() string
 }
 
 type TextOutput interface {
@@ -11,12 +17,56 @@ type TextOutput interface {
 type Game struct {
 	input TextInput
 	output TextOutput
+	player1 *player.Player
+	player2 *player.Player
 }
 
 func NewGame(userInput TextInput, output TextOutput) *Game {
 	return &Game{input: userInput, output: output}
 }
 
-func (g *Game) Start() {
+func (g *Game)FetchShipPosition() (row int, col int) {
+	userCommand := g.input.Fetch()
+
+	decimalValue, _ := strconv.Atoi(userCommand)
+
+	row = decimalValue / 10
+	col = decimalValue % 10
+
+	return row, col
+}
+
+func (g *Game) placeShips(currentPlayer *player.Player) {
+	taskOverview := fmt.Sprintf("%s - Place each of your 9 ships", currentPlayer.GetName())
+	g.output.Show(taskOverview)
+
+	for ship := 1; ship <= player.MAXIMUM_NUMBER_OF_SHIPS; ship++ {
+
+		prompt := fmt.Sprintf("%s Place Ship %d", currentPlayer.GetName(), ship)
+		g.output.Show(prompt)
+
+		row, col := g.FetchShipPosition()
+		currentPlayer.PlaceShip(row, col)
+	}
+
+	g.output.Show("")
+}
+
+func (g *Game) showWelcomeMessage() {
 	g.output.Show( "Let's play Battleships!")
+	g.output.Show("")
+}
+
+func (g *Game) askPlayersToPlaceShips() {
+	g.placeShips(g.player1)
+	g.placeShips(g.player2)
+}
+
+func (g *Game) Play() {
+	turns := player.NewTurns()
+	g.player1 = player.New(turns, "Player 1")
+	g.player2 = player.New(turns, "Player 2")
+
+	g.showWelcomeMessage()
+	g.askPlayersToPlaceShips()
 }
--- game/game_test.go
+++ game/game_test.go
@@ -7,7 +7,14 @@ import (
 
 // Stub Text Input source
 type StubTextInput struct {
-	
+	inputs []string
+	current int
+}
+
+func (t *StubTextInput) Fetch() string {
+	nextInput := t.inputs[t.current]
+	t.current++
+	return nextInput 
 }
 
 type MockTextOutput struct {
@@ -26,16 +33,107 @@ func (o *MockTextOutput) assertText(t *testing.T, want string) {
 	}
 }
 
-func TestStartGameDisplaysWelcomeMessage(t *testing.T) {
+func (o *MockTextOutput) assertAllText(t *testing.T, allWanted []string) {
+	actualIndex :=0
+
+	for _, want := range allWanted {
+		if actualIndex >= len(o.textHistory) {
+			t.Errorf("Missing output - not enough text sent. got %v", o.textHistory)
+			return
+		}
+		
+		got := o.textHistory[actualIndex]
+		actualIndex++
+
+		if got != want {
+			t.Errorf("Missing output text: got %v, want %v", got, want)
+		} 
+	}
+}
+
+func TestPlayer1WinsRepresentativeGame(t *testing.T) {
 	// Arrange
-	input := StubTextInput{}
+	allInputBothPlayers := []string{
+		// Player 1 place ship x 9
+		"10", "11", "12", "13", "14","15", "16", "20", "21",
+
+		// Player 2 place ship x 9
+		"60", "61", "62", "63", "64","65", "66", "50", "51",
+
+		// Player 1 first shot - invalid coordinates
+		"99",
+
+		// Player 1 then Player 2 shots
+		"60", "00",
+		"61", "00",
+		"62", "00",
+		"63", "00",
+		"64", "00",
+		"65", "00",
+		"50", "00",
+		"51", "00",
+		"52", "00",
+	}
+
+	input := &StubTextInput{ inputs: allInputBothPlayers }
 	output := &MockTextOutput{}
 
 	game := game.NewGame(input, output)
 
 	// Act
-	game.Start()
+	game.Play()
 
 	// Assert
-	output.assertText(t, "Let's play Battleships!")
+	want := []string{
+		"Let's play Battleships!",
+		"",
+		"Player 1 - Place each of your 9 ships",
+		"Player 1 Place Ship 1",
+		"Player 1 Place Ship 2",
+		"Player 1 Place Ship 3",
+		"Player 1 Place Ship 4",
+		"Player 1 Place Ship 5",
+		"Player 1 Place Ship 6",
+		"Player 1 Place Ship 7",
+		"Player 1 Place Ship 8",
+		"Player 1 Place Ship 9",
+		"",
+		"Player 2 - Place each of your 9 ships",
+		"Player 2 Place Ship 1",
+		"Player 2 Place Ship 2",
+		"Player 2 Place Ship 3",
+		"Player 2 Place Ship 4",
+		"Player 2 Place Ship 5",
+		"Player 2 Place Ship 6",
+		"Player 2 Place Ship 7",
+		"Player 2 Place Ship 8",
+		"Player 2 Place Ship 9",
+		"",
+	}
+
+	output.assertAllText(t, want)
+}
+
+func TestFetchesRowShipPosition(t *testing.T) {
+	// Arrange
+	input := &StubTextInput{ inputs: []string{"23"} }
+	game := game.NewGame(input, nil)
+
+	row, _ := game.FetchShipPosition()
+
+	if row != 2 {
+		t.Errorf("wrong row got %v, want 2", row)
+	}
+}
+
+func TestFetchesColumnShipPosition(t *testing.T) {
+	// Arrange
+	input := &StubTextInput{ inputs: []string{"23"} }
+	game := game.NewGame(input, nil)
+
+	_, col := game.FetchShipPosition()
+
+	if col != 3 {
+		t.Errorf("wrong col got %v, want 3", col)
+	}
 }
